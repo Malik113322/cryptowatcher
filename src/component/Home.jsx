da42937e77efe0d0c46fa0c6a1796bddbeead4b7
Malik113322 <121788489+Malik113322@users.noreply.github.com>
2023-04-06 15:02:26 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <Box w={'full'} h={'85vh'} bgColor={'blackAlpha.600'} >
 
-      <video autoPlay loop style={{ width: '100%', height: '100%', objectFit: 'cover', overflowX: 'hidden' }} src={Video}></video>
+      <video autoPlay loop muted style={{ width: '100%', height: '100%', objectFit: 'cover', overflowX: 'hidden' }} src={Video}></video>
 
       <Flex textAlign={'center'} alignItems={'center'} justifyContent={'center'}>
         <Text color={'yellow'}
